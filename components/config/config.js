b329a1d80d3b446d77788be694093a48e09e4c65
Nick Payne <nick@nickpayne.net>
2016-05-03 22:03:21 +1000

    0.19.0 FEATURE Log JavaScript errors

--- components/config/config.js
+++ components/config/config.js
@@ -3,6 +3,8 @@
 
   angular.module('SetListApp.config', [])
     .constant('API', 'https://set-lists-api-1224.appspot.com')
-    .constant('FB_APPID', '426184434182003');
+    .constant('ENV', 'production')
+    .constant('FB_APPID', '426184434182003')
+    .constant('ROLLBAR_ID', 'cb9ce30fe9934a34b6e256c84fd4c029');
 
 })();
